0c52dfc835981c63972ab03d746c7124455335bf
Josh O'Brian <joshuaeobrian@gmail.com>
2017-04-27 08:03:10 -0400

    first commit

---
+++ src/main/java/com/tiy/ballad/BalladApplication.java
@@ -0,0 +1,12 @@
+package com.tiy.ballad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalladApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalladApplication.class, args);
+	}
+}

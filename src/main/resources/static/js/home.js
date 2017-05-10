654bc598cde107273e8d5f13f1ce1dda08d0ba8e
Isaiah Anderson <ianderson@daylightdata.com>
2017-05-10 09:18:26 -0400

    added animation to home

---
+++ src/main/resources/static/js/home.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $('.color1').animate({
+    width: '70%',
+  },600);
+  $('.color2').delay(200).animate({
+    width: '70%',
+  },600);
+  $('.color3').delay(300).animate({
+    width: '70%',
+  },600);
+  $('.color4').delay(400).animate({
+    width: '70%',
+  },600);
+})
